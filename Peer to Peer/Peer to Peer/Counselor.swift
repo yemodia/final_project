25b6b008024792ade9d74271668322664562bfc9
Yerim <yerim@iMac6.local>
2019-05-13 10:16:59 -0400

    first commit

---
+++ Peer to Peer/Peer to Peer/Counselor.swift
@@ -0,0 +1,13 @@
+//
+//  Counselor.swift
+//  Peer to Peer
+//
+//  Created by Yerim on 5/13/19.
+//  Copyright © 2019 Yerim. All rights reserved.
+//
+
+import Foundation
+struct Counselor {
+    var name: String
+    var free: String
+}
---
+++ Peer to Peer/Peer to Peer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Peer to Peer
+//
+//  Created by Yerim on 5/13/19.
+//  Copyright © 2019 Yerim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
